336b8ac9845534a4ec5289144fef123a382a0f0f
Saurabh Singh <saurabhbans87@gmail.com>
2023-11-24 14:06:36 +0530

    updated

--- src/design/patterns/structural/decorator/pizza/Client.java
+++ src/design/patterns/structural/decorator/pizza/Client.java
@@ -7,12 +7,21 @@ package design.patterns.structural.decorator.pizza;
 
 public class Client {
     public static void main(String[] args) {
-        Pizza pizza = new VegExtraTopping(new VegExtraCheese(new VegPizza()));
-        System.out.println(pizza.pizza());
-        System.out.println(pizza.pizzaCost());
-
-        Pizza nonVegPizza = new NonVegExtraTopping(new NonVegExtraCheese(new NonVegPizza()));
+        System.out.println("**************************Veg Pizza*****************************");
+        Pizza vegPizza = new VegPizza();
+        System.out.println(vegPizza.pizza());
+        System.out.println(vegPizza.pizzaCost());
+        System.out.println("**************************Veg Pizza with Extra*****************************");
+        Pizza vegPizzaWithExtra = new VegExtraCheese(new VegExtraTopping(new VegPizza()));
+        System.out.println(vegPizzaWithExtra.pizza());
+        System.out.println(vegPizzaWithExtra.pizzaCost());
+        System.out.println("**************************Non Veg Pizza*****************************");
+        Pizza nonVegPizza = new NonVegPizza();
         System.out.println(nonVegPizza.pizza());
         System.out.println(nonVegPizza.pizzaCost());
+        System.out.println("**************************Non Veg Pizza with Extra*****************************");
+        Pizza nonVegPizzaWithExtra = new NonVegExtraCheese(new NonVegExtraTopping(new NonVegPizza()));
+        System.out.println(nonVegPizzaWithExtra.pizza());
+        System.out.println(nonVegPizzaWithExtra.pizzaCost());
     }
 }
