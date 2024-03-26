11201e5a19d6a58bda3a5121b942d045428136e2
Saurabh Singh <saurabhbans87@gmail.com>
2024-03-26 23:39:19 +0530

    library management system

---
+++ src/system/design/shopingkart/Product.java
@@ -0,0 +1,32 @@
+/**
+ * Create by saurabh
+ * Date: 22/03/24
+ * Project Name: Design-Patterns
+ */
+package system.design.shopingkart;
+
+public class Product {
+    private String productName;
+    private double productPrice;
+
+    public Product(String productName, double productPrice) {
+        this.productName = productName;
+        this.productPrice = productPrice;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
+}
