5da6bbba87081d10f4165641a84560deff9d2520
Saurabh Singh <saurabhbans87@gmail.com>
2024-03-26 23:39:03 +0530

    library management system

---
+++ src/system/design/socialmedia/Post.java
@@ -0,0 +1,17 @@
+/**
+ * Create by saurabh
+ * Date: 22/03/24
+ * Project Name: Design-Patterns
+ */
+package system.design.socialmedia;
+
+public class Post {
+    private String content;
+
+    public Post(String content) {
+        this.content = content;
+    }
+    public String getContent(){
+        return content;
+    }
+}
